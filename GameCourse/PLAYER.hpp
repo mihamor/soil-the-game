ee16c70c5494f5d3e7886033cdd81a97096d2106
mihamor <mishamoroz5@gmail.com>
2019-04-12 01:43:50 +0300

    added state patern

--- GameCourse/PLAYER.hpp
+++ GameCourse/PLAYER.hpp
@@ -8,16 +8,27 @@ using namespace sf;
 class Player : public Entity
 {
 private:
-	bool onGround, onLadder, shoot;
+	bool onLadder, shoot;
 	enum { stay, walk, duck, jump, climb, swim } STATE;
 	int range;
 	Slot * hand;
+	class State * current;
 
 	//hud items
 	Text amountShow;
 	Font font;
 	void playerPosNormalize();
+
+	void walkLeftPlayer();
+	void walkRightPlayer();
+	void jumpPlayer();
+	void stayPlayer();
+	void duckPlayer();
+
 public:
+	bool onGround;
+	void setCurrentState(State * state);
+	std::string getCurrentStateName();
 	std::map<std::string, bool> key;
 	Player(AnimationManager &a, int X, int Y, SoundSystem * soundSystem);
 	void setHand(Slot * bl);
@@ -30,5 +41,90 @@ public:
 	Entity * clone();
 	void Collision(int dir, String TileMap[], std::list<AbstractBlock *> blocks);
 	void drawHUD(RenderWindow & window, int vmodex, int vmodey, HudItems & items);
-	
+
+};
+
+// pattern state
+class State {
+protected:
+	std::string stateName;
+public:
+	std::string getStateName() {
+		return stateName;
+	}
+	virtual void stay(Player * p)
+	{
+		//std::cout << "   already ON\n";
+	}
+	virtual void walkLeft(Player * p)
+	{
+		//cout << "   already OFF\n";
+	}
+	virtual void walkRight(Player * p)
+	{
+		//cout << "   already OFF\n";
+	}
+	virtual void duck(Player * p)
+	{
+		//cout << "   already OFF\n";
+	}
+	virtual void jump(Player * p)
+	{
+		//cout << "   already OFF\n";
+	}
+};
+class WalkLeftState : State {
+public:
+	WalkLeftState() {
+		stateName = "walkLeft";
+	}
+	void walkLeft(Player * p);
+	void walkRight(Player * p);
+	void stay(Player * p);
+	void duck(Player * p);
+	void jump(Player * p);
+};
+class WalkRightState : State {
+public:
+	WalkRightState() {
+		stateName = "walkRight";
+	}
+	void walkLeft(Player * p);
+	void walkRight(Player * p);
+	void stay(Player * p);
+	void duck(Player * p);
+	void jump(Player * p);
+};
+class StayState : State {
+public:
+	StayState() {
+		stateName = "stay";
+	}
+	void walkLeft(Player * p);
+	void walkRight(Player * p);
+	void stay(Player * p);
+	void duck(Player * p);
+	void jump(Player * p);
+};
+class DuckState : State {
+public:
+	DuckState() {
+		stateName = "duck";
+	}
+	void walkLeft(Player * p);
+	void walkRight(Player * p);
+	void stay(Player * p);
+	void duck(Player * p);
+	void jump(Player * p);
+};
+class JumpState : State {
+public:
+	JumpState() {
+		stateName = "jump";
+	}
+	void walkLeft(Player * p);
+	void walkRight(Player * p);
+	void stay(Player * p);
+	void duck(Player * p);
+	void jump(Player * p);
 };
--- GameCourse/Player.cpp
+++ GameCourse/Player.cpp
@@ -9,6 +9,16 @@ static String intToStr(int value)
 	return result;
 }
 
+void Player::setCurrentState(State * state)
+{
+	current = state;
+}
+
+std::string Player::getCurrentStateName()
+{
+	return current->getStateName();
+}
+
 Player::Player(AnimationManager &a, int X, int Y, SoundSystem * soundSystem)
 {
 	h = a.getFrameHeight(0, "stay");
@@ -17,7 +27,8 @@ Player::Player(AnimationManager &a, int X, int Y, SoundSystem * soundSystem)
 	y = Y;
 	anim = a;
 	life = 3;
-	STATE = stay;
+	//STATE = stay;
+	setCurrentState((State* )(new StayState()));
 	name = "Player";
 	range = 5;
 	this->hand = NULL;
@@ -70,40 +81,41 @@ void Player::KeyCheck()
 
 	if (key["L"])
 	{
-		dir = 1;
-		if (STATE != duck) dx = -0.1;
-		if (STATE == stay) STATE = walk;
+		walkLeftPlayer();
+		//dir = 1;
+		//if (STATE != duck) dx = -0.1;
+		//if (STATE == stay) STATE = walk;
 	}
 	if (key["R"])
-	{
-		dir = 0;
-		if (STATE != duck) dx = 0.1;
-		if (STATE == stay) STATE = walk;
+	{	
+		walkRightPlayer();
+		//dir = 0;
+		//if (STATE != duck) dx = 0.1;
+		//if (STATE == stay) STATE = walk;
 	}
 
 	if (key["Up"])
 	{
-		if (STATE == stay || STATE == walk) { dy = -0.4; STATE = jump; this->soundHandler->play("player_jump");  }
-		if (STATE == swim || STATE == climb) dy = -0.05;
+		jumpPlayer();
+		//if (STATE == stay || STATE == walk) { dy = -0.4; STATE = jump; this->soundHandler->play("player_jump");  }
+		//if (STATE == swim || STATE == climb) dy = -0.05;
 	}
 	if (key["Down"])
 	{
-		if (STATE == stay) { dx = 0; STATE = duck; }
-		if (STATE == swim || STATE == climb) dy = 0.05;
+		duckPlayer();
+		//if (STATE == stay) { dx = 0; STATE = duck; }
+		//if (STATE == swim || STATE == climb) dy = 0.05;
 	}
 	if (key["Space"])
 		shoot = true;
 
 	// otpuskanie klavish
-	if (!(key["R"] || key["L"]))
+	if (!key["R"] && !key["L"] && !key["Down"] && !key["Up"])
 	{
-		dx = 0;
-		if (STATE == walk) STATE = stay;
+		if(getCurrentStateName() != "jump") stayPlayer();
+		//dx = 0;
+		//if (STATE == walk) STATE = stay;
 	}
-	if (!(key["Up"] || key["Down"]))
-		if (STATE == swim || STATE == climb) dy = 0;
-	if (!key["Down"])
-		if (STATE == duck) STATE = stay;
 	if (!key["Space"])
 		shoot = false;
 }
@@ -115,26 +127,27 @@ void Player::update(float time, String TileMap[], std::list<AbstractBlock *> blo
 {
 
 	if(!this->hit)KeyCheck();
-	if (STATE == stay) anim.set("stay");
-	if (STATE == walk) anim.set("walk");
-	if (STATE == jump) {
-		anim.set("jump");
-	}
-	if (STATE == duck) anim.set("duck");
-	if (STATE == climb)
-	{
+	//if (STATE == stay) anim.set("stay");
+	//if (STATE == walk) anim.set("walk");
+	//if (STATE == jump) {
+	//	anim.set("jump");
+	//}
+	//if (STATE == duck) anim.set("duck");
+	//if (STATE == climb)
+	//{
 		//		anim.set("climb");
-		anim.pause();
-		if (dy != 0) anim.play();
-		if (!onLadder) STATE = stay;
-	}
+	//	anim.pause();
+	//	if (dy != 0) anim.play();
+	//	if (!onLadder) STATE = stay;
+	//}
 	if (shoot)
 	{
 		anim.set("shoot");
-		if (STATE == walk) anim.set("shootAndWalk");
+		//if (STATE == walk) anim.set("shootAndWalk");
 	}
 	anim.flip(dir);
 
+	//std::cout << "dx: " << dx << std::endl;
 	float encrease = dx * time;
 	int encLimit = 32;
 	if (fabs(encrease) > encLimit) { encrease = encrease > 0 ? encLimit : -encLimit; std::cout << "Enc Limit hit!" << std::endl; }
@@ -163,6 +176,46 @@ void Player::playerPosNormalize() {
 	this->y = 60;
 }
 
+void Player::walkLeftPlayer()
+{
+	if (current->getStateName() != "walkLeft") {
+		std::cout << "walkLeft" << std::endl;
+		current->walkLeft(this);
+	}
+}
+
+void Player::walkRightPlayer()
+{
+	if (current->getStateName() != "walkRight") {
+		std::cout << "walkRight" << std::endl;
+		current->walkRight(this);
+	}
+}
+
+void Player::jumpPlayer()
+{
+	if (current->getStateName() != "jump") {
+		std::cout << "jump" << std::endl;
+		current->jump(this);
+	}
+}
+
+void Player::stayPlayer()
+{
+	if (current->getStateName() != "stay") {
+		std::cout << "stay" << std::endl;
+		current->stay(this);
+	}
+}
+
+void Player::duckPlayer()
+{
+	if (current->getStateName() != "duck") {
+		std::cout << "duck" << std::endl;
+		current->duck(this);
+	}
+}
+
 void Player::Collision(int dir, String TileMap[], std::list<AbstractBlock *> blocks)
 {
 	for (int i = y / 32; i < (y + h) / 32; i++)
@@ -180,7 +233,8 @@ void Player::Collision(int dir, String TileMap[], std::list<AbstractBlock *> blo
 					y = i * 32 - h;
 					dy = 0;
 					onGround = true;
-					STATE = stay;
+
+					if(getCurrentStateName() == "jump") stayPlayer();
 
 					if (hit) {
 						hit = false;
@@ -226,4 +280,182 @@ void Player::drawHUD(RenderWindow & window, int vmodex, int vmodey, HudItems & i
 		window.draw(inHand);
 		window.draw(amountShow);
 	}
+}
+
+
+void setValuesToJump(Player * p) {
+	p->dy = -0.4;
+	p->onGround = false;
+	p->soundHandler->play("player_jump");
+	p->anim.set("jump");
+}
+void setValuesToStay(Player * p) {
+	p->dx = 0;
+	p->anim.set("stay");
+}
+void setValuesToDuck(Player * p) {
+	p->dx = 0;
+	p->anim.set("duck");
+}
+void setValuesToWalk(Player * p, int dir) {
+	p->dx = dir ? -0.1 : 0.1;
+	p->dir = dir;
+	p->anim.set("walk");
+}
+
+void WalkRightState::walkLeft(Player * p)
+{
+	setValuesToWalk(p, 1);
+	p->setCurrentState((State *)(new WalkLeftState()));
+	//p->dx = p->dir ? -0.1 : 0.1;
+	//p->anim.set("walk");
+	//set values
+}
+void WalkRightState::walkRight(Player * p)
+{
+	std::cout << "Already in walkRight state" << std::endl;
+	
+	//p->dx = p->dir ? -0.1 : 0.1;
+	//p->anim.set("walk");
+	//set values
+}
+
+void WalkRightState::stay(Player * p)
+{
+	setValuesToStay(p);
+	p->setCurrentState((State *)(new StayState()));
+}
+void WalkRightState::duck(Player * p)
+{
+	setValuesToDuck(p);
+	p->setCurrentState((State *)(new DuckState()));
+}
+void WalkRightState::jump(Player * p)
+{
+	setValuesToJump(p);
+	p->setCurrentState((State *)(new JumpState()));
+}
+///////////////////////////////////
+
+
+void WalkLeftState::walkLeft(Player * p)
+{
+	std::cout << "Already in walkLeft state" << std::endl;
+	//p->dx = p->dir ? -0.1 : 0.1;
+	//p->anim.set("walk");
+	//set values
+}
+void WalkLeftState::walkRight(Player * p)
+{
+	setValuesToWalk(p, 0);
+	p->setCurrentState((State *)(new WalkRightState()));
+	//p->dx = p->dir ? -0.1 : 0.1;
+	//p->anim.set("walk");
+	//set values
+}
+
+void WalkLeftState::stay(Player * p)
+{
+	setValuesToStay(p);
+	p->setCurrentState((State *)(new StayState()));
+}
+void WalkLeftState::duck(Player * p)
+{
+	setValuesToDuck(p);
+	p->setCurrentState((State *)(new DuckState()));
+}
+void WalkLeftState::jump(Player * p)
+{
+	setValuesToJump(p);
+	p->setCurrentState((State *)(new JumpState()));
+}
+///////////////////////////////////
+void StayState::walkLeft(Player * p)
+{
+	setValuesToWalk(p, 1);
+	p->setCurrentState((State *)(new WalkLeftState()));
+}
+void StayState::walkRight(Player * p)
+{
+	setValuesToWalk(p, 0);
+	p->setCurrentState((State *)(new WalkRightState()));
+}
+void StayState::stay(Player * p)
+{
+	std::cout << "Already in stay state" << std::endl;
+	//p->dx = 0;
+	//p->anim.set("stay");
+	//set values
+}
+void StayState::duck(Player * p)
+{
+	setValuesToDuck(p);
+	p->setCurrentState((State *)(new DuckState()));
+}
+void StayState::jump(Player * p)
+{
+	setValuesToJump(p);
+	p->setCurrentState((State *)(new JumpState()));
+}
+/////////////////////
+void DuckState::walkLeft(Player * p)
+{
+	setValuesToWalk(p, 1);
+	p->setCurrentState((State *)(new WalkLeftState()));
+}
+void DuckState::walkRight(Player * p)
+{
+	setValuesToWalk(p, 0);
+	p->setCurrentState((State *)(new WalkRightState()));
+}
+
+
+void DuckState::stay(Player * p)
+{
+	setValuesToStay(p);
+	p->setCurrentState((State *)(new StayState()));
+}
+void DuckState::duck(Player * p)
+{
+	//p->dx = 0;
+	//p->anim.set("duck");
+	// set values
+	std::cout << "Already in duck state" << std::endl;
+}
+void DuckState::jump(Player * p)
+{
+	setValuesToJump(p);
+	p->setCurrentState((State *)(new JumpState()));
+}
+///////////////////////////////
+void JumpState::walkLeft(Player * p)
+{
+	p->dir = 1;
+	p->dx = -0.1;
+	//p->setCurrentState((State *)(new WalkLeftState()));
+}
+void JumpState::walkRight(Player * p)
+{
+	p->dir = 0;
+	p->dx = 0.1;
+	//p->setCurrentState((State *)(new WalkLeftState()));
+}
+void JumpState::stay(Player * p)
+{
+	setValuesToStay(p);
+	p->setCurrentState((State *)(new StayState()));
+}
+void JumpState::duck(Player * p)
+{
+	setValuesToDuck(p);
+	p->setCurrentState((State *)(new DuckState()));
+}
+void JumpState::jump(Player * p)
+{
+	std::cout << "Already in jump state" << std::endl;
+	//p->dy = -0.4;
+	//p->onGround = false;
+	//p->soundHandler->play("player_jump");
+	//p->anim.set("jump");
+	//set value
 }
